4d3fa3512818841b4b7c5c6619ecb2ea76331ff2
Puleeno Nguyen <puleeno@gmail.com>
2018-10-10 08:00:16 +0700

    Fix typo variable name

--- trait-foxy-meta.php
+++ trait-foxy-meta.php
@@ -3,7 +3,7 @@
 trait Foxy_Meta {
 	protected $meta_framework;
 
-
+x
 	public function meta($meta_key, $id = null, $meta_type = 'post') {
 		return get_metadata($meta_type, $id, $meta_key);
 	}
@@ -13,6 +13,6 @@ trait Foxy_Meta {
 			$user = wp_get_current_user();
 			$user_id = $user->ID();
 		}
-		return get_metadata('user', $id, $meta_key);
+		return get_metadata('user', $user_id, $meta_key);
 	}
 }
