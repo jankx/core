5f1be44d685904bd31a92b3075afc556cb668a21
Puleeno Nguyen <puleeno@gmail.com>
2018-10-10 07:57:04 +0700

    update class data register

--- class-foxy-data.php
+++ class-foxy-data.php
@@ -12,8 +12,49 @@
  * Foxy Data class
  */
 class Foxy_Data {
-    protected static $instance;
+	protected static $instance;
 
-    public static function instance() {
-    }
+	public static function instance() {
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
+	public function __construct() {
+		$this->register_posts_types();
+		$this->register_taxonomies();
+		$this->add_post_metas();
+		$this->add_user_metas();
+	}
+
+	public function register_posts_types() {
+		$post_types = apply_filters( 'foxy_post_types', array() );
+		if ( ! empty( $post_types ) && is_array( $post_types ) ) {
+			foreach ( $post_types as $post_type => $args ) {
+				register_post_type( $post_type, $args );
+			}
+		}
+	}
+
+	public function register_taxonomies() {
+		$taxonomies = apply_filters( 'foxy_taxonomies', array() );
+		if ( ! empty( $taxonomies ) && is_array( $taxonomies ) ) {
+
+		}
+	}
+
+	public function add_post_metas() {
+		$meta_boxes = apply_filters( 'foxy_meta_boxes', array() );
+		if ( ! empty( $meta_boxes ) && is_array( $meta_boxes ) ) {
+
+		}
+	}
+
+	public function add_user_metas() {
+		$user_metas = apply_filters( 'foxy_user_metas', array() );
+		if ( ! empty( $user_metas ) && is_array( $user_metas ) ) {
+
+		}
+	}
 }
--- trait-foxy-meta.php
+++ trait-foxy-meta.php
@@ -2,4 +2,17 @@
 
 trait Foxy_Meta {
 	protected $meta_framework;
+
+
+	public function meta($meta_key, $id = null, $meta_type = 'post') {
+		return get_metadata($meta_type, $id, $meta_key);
+	}
+
+	public function user_meta($meta_key, $user_id = null) {
+		if ( is_null( $user_id ) ) {
+			$user = wp_get_current_user();
+			$user_id = $user->ID();
+		}
+		return get_metadata('user', $id, $meta_key);
+	}
 }
