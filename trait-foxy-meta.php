4ce98bb3a0d0c6a6bc7efc62e4a095cc4c6619f1
Puleeno Nguyen <puleeno@gmail.com>
2018-10-10 10:41:51 +0700

    Update wordpress coding standard

--- trait-foxy-meta.php
+++ trait-foxy-meta.php
@@ -1,18 +1,55 @@
 <?php
+/**
+ * WordPress has define meta to extends Post infos and User info
+ * Foxy will use meta info in a different way
+ *
+ * @package Foxy/Core
+ * @author Puleeno Nguyen <puleeno@gmail.com>
+ * @link https://wpclouds.com
+ */
 
+/**
+ * Foxy_Meta trait
+ */
 trait Foxy_Meta {
+	/**
+	 * WordPress Meta Framework integrate with Foxy theme framework
+	 *
+	 * @var instaceof Foxy_Meta_Base
+	 */
 	protected $meta_framework;
 
-x
-	public function meta($meta_key, $id = null, $meta_type = 'post') {
-		return get_metadata($meta_type, $id, $meta_key);
+	/**
+	 * Get meta value via meta framework
+	 * Every meta framework has different method to get meta value
+	 * So Foxy framework must integrate with meta framework is used
+	 *
+	 * @param string $meta_key   Meta key need to get value.
+	 * @param int    $id         Post or User ID need to get meta value.
+	 * @param string $meta_type  Meta type may be is post or user.
+	 *
+	 * @return string
+	 */
+	public function meta( $meta_key, $id = null, $meta_type = 'post' ) {
+		if ( is_null( $id ) ) {
+			$id = get_the_ID();
+		}
+		return get_metadata( $meta_type, $id, $meta_key );
 	}
 
-	public function user_meta($meta_key, $user_id = null) {
+	/**
+	 * Get user meta from WordPress user meta
+	 *
+	 * @param string $meta_key  Meta key need to get meta value.
+	 * @param int    $user_id   User ID need to get meta value.
+	 *
+	 * @return string
+	 */
+	public function user_meta( $meta_key, $user_id = null ) {
 		if ( is_null( $user_id ) ) {
-			$user = wp_get_current_user();
+			$user    = wp_get_current_user();
 			$user_id = $user->ID();
 		}
-		return get_metadata('user', $user_id, $meta_key);
+		return get_metadata( 'user', $user_id, $meta_key );
 	}
 }
