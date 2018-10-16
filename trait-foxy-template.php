c9f98ded95119ebbdc1a457a7920f458c702834c
Puleeno Nguyen <puleeno@gmail.com>
2018-10-17 01:36:07 +0700

    Update PHPDoc and WordPress coding standard

--- trait-foxy-template.php
+++ trait-foxy-template.php
@@ -1,6 +1,43 @@
 <?php
+/**
+ * Foxy Template Helpers
+ * This file define template methods use to load template files
+ * from `templates` directory in theme folder
+ *
+ * @package Foxy/Core
+ * @subpackage UI
+ * @author Puleeno Nguyen <puleeno@gmail.com>
+ * @link https://wpclouds.com
+ */
 
+/**
+ * Foxy_Template trait
+ */
 trait Foxy_Template {
+	/**
+	 * Search template path
+	 *
+	 * @param array|string $template_names Template name(s) need to search location.
+	 * @return string
+	 */
+	public static function search_template( $template_names ) {
+		$located = '';
+		foreach ( (array) $template_names as $template_name ) {
+			if ( ! $template_name ) {
+				continue;
+			}
+			$template_name = 'templates/' . $template_name;
+			if ( file_exists( FOXY_ACTIVE_THEME_DIR . $template_name ) ) {
+				$located = FOXY_ACTIVE_THEME_DIR . $template_name;
+				break;
+			} elseif ( file_exists( FOXY_TEMPLATE_DIR . $template_name ) ) {
+				$located = FOXY_TEMPLATE_DIR . $template_name;
+				break;
+			}
+		}
+		return $located;
+	}
+
 	/**
 	 * Check template is exists
 	 *
@@ -24,29 +61,5 @@ trait Foxy_Template {
 		if ( '' !== $located ) {
 			load_template( $located, $require_once );
 		}
-	}
-
-	/**
-	 * Search template path
-	 *
-	 * @param array|string $template_names Template name(s) need to search location.
-	 * @return string
-	 */
-	public static function search_template( $template_names ) {
-		$located = '';
-		foreach ( (array) $template_names as $template_name ) {
-			if ( ! $template_name ) {
-				continue;
-			}
-			$template_name = 'templates/' . $template_name;
-			if ( file_exists( FOXY_ACTIVE_THEME_DIR . $template_name ) ) {
-				$located = FOXY_ACTIVE_THEME_DIR . $template_name;
-				break;
-			} elseif ( file_exists( FOXY_TEMPLATE_DIR . $template_name ) ) {
-				$located = FOXY_TEMPLATE_DIR . $template_name;
-				break;
-			}
-		}
-		return $located;
 	}
 }
