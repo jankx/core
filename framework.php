d4de9114397dce2cf2d979a15ad66f58769c5474
Puleeno Nguyen <puleeno@gmail.com>
2018-10-22 16:54:14 +0700

    Add framework compatibility

--- framework.php
+++ framework.php
@@ -1,11 +1,31 @@
 <?php
+/**
+ * Foxy Framework Core
+ *
+ * @package Foxy/Core
+ * @author Puleeno Nguyen <puleeno@gmail.com>
+ * @license GPL
+ * @link https://wpclouds.com
+ */
 
-define( 'FOXY_FRAMEWORK_FILE', __FILE__ );
+/**
+ * Check Foxy framework don't have registered
+ */
+if ( ! class_exists( 'Foxy' ) && ! defined( 'FOXY_FRAMEWORK_FILE' ) ) {
+	define( 'FOXY_FRAMEWORK_FILE', __FILE__ );
 
-require_once dirname( FOXY_FRAMEWORK_FILE ) . '/class-foxy-setup.php';
+	require_once dirname( FOXY_FRAMEWORK_FILE ) . '/class-foxy-setup.php';
 
-Foxy_Setup::initialize();
+	Foxy_Setup::initialize();
+}
 
-function foxy() {
-	return $GLOBALS['foxy'];
+if ( ! function_exists( 'foxy' ) ) {
+	/**
+	 * Helper get Foxy instance from $GLOBALS variable
+	 *
+	 * @return Foxy
+	 */
+	function foxy() {
+		return $GLOBALS['foxy'];
+	}
 }
