53f5603d4546fa6e0128980fe902de20f6a21379
Puleeno Nguyen <puleeno@gmail.com>
2018-11-06 08:12:55 +0700

    Fix post layout

--- class-foxy-post-layout.php
+++ class-foxy-post-layout.php
@@ -10,23 +10,28 @@ class Foxy_Post_Layout {
 			throw new Exception( 'Argument #1 must be instance of WP_Query' );
 		}
 		$args = wp_parse_args( $args, array(
-			'style' => 'list',
+			'style' => 'card',
+			'row_items' => 4,
 			'carousel' => false,
 		));
-		$has_carousel = $args['carousel'] ? ' has-carousel' : '';
 		$style = $args['style'];
+		$has_carousel = $args['carousel'] ? 'has-carousel' : 'not-carousel';
+
+		$class_names = array(
+			'post-layout',
+			$has_carousel,
+			sprintf( '%s-layout', $style ),
+			sprintf( 'row-item-%s', $args['row_items'] ),
+		);
 
 		Foxy::ui()->tag(array(
-			'class' => sprintf(
-				'post-layout post-layout-%1$s style-%1$s%2$s',
-				$style, $has_carousel
-			),
+			'class' => implode( ' ', $class_names ),
 		));
 		$wrap_class = $args['carousel'] ? 'post-layout-wrap owl-carousel' : 'post-layout-wrap';
 		if ( $posts->have_posts() ) {
 			Foxy::ui()->tag(array(
 				'name' => 'section',
-				'class' => $wrap_class
+				'class' => $wrap_class,
 			));
 			do_action( 'foxy_post_layout_before_loop' );
 			do_action( "foxy_post_layout_{$style}_before_loop" );
@@ -48,10 +53,14 @@ class Foxy_Post_Layout {
 
 			do_action( "foxy_post_layout_{$style}_end_loop" );
 			do_action( 'foxy_post_layout_after_loop' );
-			Foxy::ui()->tag(array(
-				'name' => 'section',
-				'close' => 'true'
-			));
+
+			echo '<div class="clearfix"></div>';
+			Foxy::ui()->tag(
+				array(
+					'name' => 'section',
+					'close' => 'true',
+				)
+			);
 		} else {
 			foxy_no_content();
 		}
