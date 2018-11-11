d18c6710fe8279dcceae29c4b90de18e99ec092a
Puleeno Nguyen <puleeno@gmail.com>
2018-11-11 09:34:36 +0700

    Add function array_columns if not exists

--- function-wordpress-compatibility.php
+++ function-wordpress-compatibility.php
@@ -38,3 +38,14 @@ function foxy_allowed_protocols( $protocols ) {
 
 	return $protocols;
 }
+
+if ( ! function_exists( 'array_column' ) ) {
+    function array_column( $array, $column_name ) {
+        return array_map(
+			function ( $element ) use( $column_name ) {
+				return $element[ $column_name ];
+			},
+			$array
+		);
+    }
+}
