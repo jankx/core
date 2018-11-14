4fffcb7d309cd8645374ba6dbde6059ca9a8e8e4
Puleeno Nguyen <puleeno@gmail.com>
2018-11-14 17:47:01 +0700

    Update option framework for foxy

--- class-foxy-option-framework-foxy.php
+++ class-foxy-option-framework-foxy.php
@@ -9,10 +9,20 @@
  * Foxy_Option_Framework_Foxy class
  */
 class Foxy_Option_Framework_Foxy extends Foxy_Option_Framework_Base {
+	protected $factory;
+
+	public function __construct() {
+		parent::__construct();
+		$this->factory = Foxy_Fields_Factory_Option();
+	}
+
+	public function load_options( $id, $refresh = flase ) {
+	}
+
 	public function get_option( $option_name, $default_value = false ) {
 		return $default_value;
 	}
 
-	public function update_option( $option_name, $option_value ) {
+	public function add_section( $fields ) {
 	}
 }
---
+++ interface-foxy-option-framework-interface.php
@@ -0,0 +1,17 @@
+<?php
+interface Foxy_Option_Framework_Interface {
+	public function id( $id );
+
+	public function load_options( $id, $refresh = false );
+
+	/**
+	 * Get option value by option name
+	 *
+	 * @param string  $option_name Option name.
+	 * @param boolean $default_value Default value when option name don't have value.
+	 * @return mixed
+	 */
+	public function get_option( $option_name, $default_value = false );
+
+	public function add_section( $fields );
+}
