26d29d9a3204f2cff709a5aa281bce7f1a608e1a
Puleeno Nguyen <puleeno@gmail.com>
2018-11-15 22:45:08 +0700

    Update option framework

--- class-foxy-option-framework-base.php
+++ class-foxy-option-framework-base.php
@@ -29,8 +29,45 @@ abstract class Foxy_Option_Framework_Base implements Foxy_Option_Framework_Inter
 		$this->load_options( $this->id );
 	}
 
-	public function id( $id ) {
+	public function id( $id = null ) {
+		if ( is_null( $id ) ) {
+			return $this->id;
+		}
 		$this->id = $id;
 		return $this;
 	}
+
+	public function admin_page() {
+		$option_files = $this->search_option_files();
+		foreach ( $option_files as $option_file ) {
+			$fields = require $option_file;
+			if ( ! is_array( $fields ) ) {
+				continue;
+			}
+			$this->add_sections( $fields );
+		}
+	}
+
+	private function search_option_files() {
+		$search_directories = apply_filters(
+			'foxy_options_directories',
+			array(
+				sprintf( '%soptions', FOXY_TEMPLATE_DIR ),
+				sprintf( '%sincludes/options', FOXY_TEMPLATE_DIR ),
+				sprintf( '%soptions', FOXY_ACTIVE_THEME_DIR ),
+				sprintf( '%sincludes/options', FOXY_ACTIVE_THEME_DIR ),
+			)
+		);
+		$option_files       = array();
+		foreach ( (array) $search_directories as $directory ) {
+			if ( ! file_exists( $directory ) ) {
+				continue;
+			}
+			$searched_files = glob( $directory . '/{*.php,*/*.php}', GLOB_BRACE );
+			$option_files   = array_merge_recursive( $option_files, $searched_files );
+		}
+		return $option_files;
+	}
+
+
 }
--- class-foxy-option-framework-redux.php
+++ class-foxy-option-framework-redux.php
@@ -1,15 +1,20 @@
 <?php
 class Foxy_Option_Framework_Redux extends Foxy_Option_Framework_Base {
 	public function load_options( $id, $refresh = flase ) {
+		$this->loaded_options[ $id ] = $GLOBALS[ $this->id() ];
 	}
 
 	public function get_option( $option_name, $default_value = false ) {
 		return $default_value;
 	}
 
-	public function add_section( $fields ) {
-	}
-
-	public function admin_page() {
+	public function add_sections( $sections ) {
+		if ( isset( $sections[0] ) && is_array( $sections[0] ) ) {
+			foreach ( $sections as $section ) {
+				Redux::setSection( $this->id, $section );
+			}
+		} else {
+			Redux::setSection( $this->id, $section );
+		}
 	}
 }
