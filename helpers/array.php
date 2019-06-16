ad2c42a8a42abb09a5509731cfa17a57519560b6
Puleeno Nguyen <puleeno@gmail.com>
2019-06-17 03:19:35 +0700

    Added helpers structure for Jankx core

---
+++ helpers/array.php
@@ -0,0 +1,16 @@
+<?php
+function array_get($arr, $arrayIndex, $defaultValue = null)
+{
+    if (is_string($arrayIndex)) {
+        $arrayIndex = explode('.', $arrayIndex);
+    } else {
+        $arrayIndex = (array)$arrayIndex;
+    }
+    foreach ($arrayIndex as $index) {
+        if (!isset($arr[$index])) {
+            return $defaultValue;
+        }
+        $arr = $arr[$index];
+    }
+    return $arr;
+}
