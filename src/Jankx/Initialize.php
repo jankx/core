d62e53c36a18aba65749d7c523abfa689188d1b8
Puleeno Nguyen <puleeno@gmail.com>
2019-06-16 22:32:27 +0700

    Added site layout to core

--- src/Jankx/Initialize.php
+++ src/Jankx/Initialize.php
@@ -1,11 +1,17 @@
 <?php
 namespace Jankx;
 
+use Jankx\SiteLayouts\Layout as SiteLayout;
+
 class Initialize
 {
     public static function init()
     {
         self::themeSupports();
+
+        if (class_exists(SiteLayout::class)) {
+            $GLOBALS['site_layout'] = SiteLayout::instance();
+        }
     }
 
     /**
