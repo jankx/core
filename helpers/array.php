a0351dc19d9b01632e007f67a80dee6718c935a0
Puleeno Nguyen <puleeno@gmail.com>
2019-10-14 00:04:33 +0700

    Integrated with other plugins

--- helpers/array.php
+++ helpers/array.php
@@ -1,16 +1,18 @@
 <?php
-function array_get($arr, $arrayIndex, $defaultValue = null)
-{
-    if (is_string($arrayIndex)) {
-        $arrayIndex = explode('.', $arrayIndex);
-    } else {
-        $arrayIndex = (array)$arrayIndex;
-    }
-    foreach ($arrayIndex as $index) {
-        if (!isset($arr[$index])) {
-            return $defaultValue;
-        }
-        $arr = $arr[$index];
-    }
-    return $arr;
+if (!function_exists('array_get')) {
+	function array_get($arr, $arrayIndex, $defaultValue = null)
+	{
+		if (is_string($arrayIndex)) {
+			$arrayIndex = explode('.', $arrayIndex);
+		} else {
+			$arrayIndex = (array)$arrayIndex;
+		}
+		foreach ($arrayIndex as $index) {
+			if (!isset($arr[$index])) {
+				return $defaultValue;
+			}
+			$arr = $arr[$index];
+		}
+		return $arr;
+	}
 }
