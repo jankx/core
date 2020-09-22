14b79c0b41b6c703401d4d2d80c2e070cab1135a
Puleeno Nguyen <puleeno@gmail.com>
2020-09-22 13:48:24 +0700

    Integrate Elementor container with theme

--- src/Jankx/Integrate/Elementor/Elementor.php
+++ src/Jankx/Integrate/Elementor/Elementor.php
@@ -19,6 +19,18 @@ class Elementor extends Constract
         if (apply_filters('jankx_plugin_elementor_silent_mode', false)) {
             add_filter('elementor/editor/localize_settings', array( $this, 'removeElementPromtionWidgets'));
         }
+
+        add_action('template_redirect', array($this, 'integrateTemplateClasses'));
+    }
+
+    public function integrateTemplateClasses()
+    {
+        if (is_singular() && \Elementor\Plugin::instance()->db->is_built_with_elementor(get_the_ID())) {
+            add_action('jankx_template_before_open_container', array($this, 'openElementorSelectionClass'));
+            add_action('jankx_template_after_close_container', array($this, 'closeElementorSelectionClass'));
+
+            add_filter('jankx_template_the_container_classes', array($this, 'addElementorContainerClass'));
+        }
     }
 
     public function registerJankxCategory($elementsManager)
@@ -74,5 +86,26 @@ class Elementor extends Constract
 
         $widgetCategories = array_merge($highPriorityCategories, $widgetCategories);
         $widgetCategoryRefProp->setValue($elementManager, $widgetCategories);
+    }
+
+    public function openElementorSelectionClass()
+    {
+        echo '<div class="elementor-section elementor-section-boxed">';
+    }
+
+    public function closeElementorSelectionClass()
+    {
+        echo '</div>';
+    }
+
+    public function addElementorContainerClass($classes)
+    {
+        if (is_string($classes)) {
+            $classes = array($classes);
+        }
+        // Added elementor container class to theme
+        $classes[] = 'elementor-container';
+
+        return $classes;
     }
 }
