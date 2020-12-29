54e2dd281e9997e5f5ed26b5635a1bd823f07731
Puleeno Nguyen <puleeno@gmail.com>
2020-12-29 21:12:53 +0700

    Init class ConfigurationReader

---
+++ src/Jankx/ConfigurationReader.php
@@ -0,0 +1,10 @@
+<?php
+namespace Jankx;
+/**
+ * Configurations reader
+ *
+ * This class use to read file theme.yml in root directory of theme.
+ * Child theme can override configs
+ */
+class ConfigurationReader {
+}
