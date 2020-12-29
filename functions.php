cb5b3a8287a65a09e813f949c7676cfc110262b2
Puleeno Nguyen <puleeno@gmail.com>
2020-12-29 13:20:02 +0700

    Create new function jankx_get_related_query

--- functions.php
+++ functions.php
@@ -1,1 +1,53 @@
 <?php
+function jankx_get_related_query($args = array(), $post_id = null)
+{
+    if (is_null($post_id)) {
+        global $post;
+        $post_id = $post->ID;
+    }
+    $args      = wp_parse_args(
+        $args,
+        array(
+            'post_type' => 'post',
+        )
+    );
+    $tax_query = array();
+
+    if ('post' === $args['post_type']) {
+        $category_ids = wp_get_post_categories(
+            $post_id,
+            array( 'fields' => 'ids' )
+        );
+        if (! empty($category_ids)) {
+            $tax_query[] = array(
+                'taxonomy' => 'category',
+                'field'    => 'id',
+                'terms'    => $category_ids,
+            );
+        }
+
+        $tag_ids = wp_get_post_tags(
+            $post_id,
+            array( 'fields' => 'ids' )
+        );
+        if (! empty($tag_ids)) {
+            $tax_query[] = array(
+                'taxonomy' => 'post_tag',
+                'field'    => 'id',
+                'terms'    => $tag_ids,
+            );
+        }
+    }
+
+    if (! empty($tax_query)) {
+        $tax_query['relation'] = 'OR';
+        $args['tax_query']     = $tax_query;
+    }
+
+    return new WP_Query(
+        apply_filters(
+            'jankx_related_query_args',
+            $args
+        ),
+    );
+}
