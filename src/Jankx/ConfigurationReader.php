891778bb9047f8d20729e5209b2f648becb2d5e3
Puleeno Nguyen <puleeno@gmail.com>
2020-12-31 08:18:53 +0700

    Fix coding standard

--- src/Jankx/ConfigurationReader.php
+++ src/Jankx/ConfigurationReader.php
@@ -1,10 +1,12 @@
 <?php
 namespace Jankx;
+
 /**
  * Configurations reader
  *
  * This class use to read file theme.yml in root directory of theme.
  * Child theme can override configs
  */
-class ConfigurationReader {
+class ConfigurationReader
+{
 }
