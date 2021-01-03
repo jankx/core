5259ddde87e067d91d58eb02b32c1b3525994dd3
Puleeno Nguyen <puleeno@gmail.com>
2021-01-03 12:38:57 +0700

    Split comment item to template file

--- src/Jankx/Walker/CommentWalker.php
+++ src/Jankx/Walker/CommentWalker.php
@@ -25,119 +25,55 @@ class CommentWalker extends Walker_Comment
      */
     protected function html5_comment($comment, $depth, $args)
     {
-
+        // Open comment item
         $tag = ( 'div' === $args['style'] ) ? 'div' : 'li';
+        printf(
+            '<%s id="comment-%d" %s>',
+            $tag,
+            $comment->ID,
+            comment_class(
+                $this->has_children ? 'parent' : '',
+                $comment,
+                null,
+                false
+            )
+        );
 
-        ?>
-        <<?php echo $tag; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static output ?> id="comment-<?php comment_ID(); ?>" <?php comment_class($this->has_children ? 'parent' : '', $comment); ?>>
-            <article id="div-comment-<?php comment_ID(); ?>" class="comment-body">
-                <footer class="comment-meta">
-                    <div class="comment-author vcard">
-                        <?php
-                        $comment_author_url = get_comment_author_url($comment);
-                        $comment_author     = get_comment_author($comment);
-                        $avatar             = get_avatar($comment, $args['avatar_size']);
-                        if (0 !== $args['avatar_size']) {
-                            if (empty($comment_author_url)) {
-                                echo wp_kses_post($avatar);
-                            } else {
-                                printf('<a href="%s" rel="external nofollow" class="url">', $comment_author_url); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped --Escaped in https://developer.wordpress.org/reference/functions/get_comment_author_url/
-                                echo wp_kses_post($avatar);
-                            }
-                        }
+        $comment_author_url = get_comment_author_url($comment);
+        $comment_author     = get_comment_author($comment);
+        $avatar             = get_avatar($comment, $args['avatar_size']);
+        $comment_timestamp  = sprintf(__('%1$s at %2$s', 'jankx'), get_comment_date('', $comment), get_comment_time());
+        $comment_reply_link = get_comment_reply_link(
+            array_merge(
+                $args,
+                array(
+                    'add_below' => 'div-comment',
+                    'depth'     => $depth,
+                    'max_depth' => $args['max_depth'],
+                    'before'    => '<span class="comment-reply">',
+                    'after'     => '</span>',
+                )
+            )
+        );
+        $by_post_author   = jankx_is_comment_by_post_author($comment);
 
-                        printf(
-                            '<span class="fn">%1$s</span><span class="screen-reader-text says">%2$s</span>',
-                            esc_html($comment_author),
-                            __('says:', 'jankx')
-                        );
-
-                        if (! empty($comment_author_url)) {
-                            echo '</a>';
-                        }
-                        ?>
-                    </div><!-- .comment-author -->
-
-                    <div class="comment-metadata">
-                        <?php
-                        /* translators: 1: Comment date, 2: Comment time. */
-                        $comment_timestamp = sprintf(__('%1$s at %2$s', 'jankx'), get_comment_date('', $comment), get_comment_time());
-
-                        printf(
-                            '<a href="%s"><time datetime="%s" title="%s">%s</time></a>',
-                            esc_url(get_comment_link($comment, $args)),
-                            get_comment_time('c'),
-                            esc_attr($comment_timestamp),
-                            esc_html($comment_timestamp)
-                        );
-
-                        if (get_edit_comment_link()) {
-                            printf(
-                                ' <span aria-hidden="true">&bull;</span> <a class="comment-edit-link" href="%s">%s</a>',
-                                esc_url(get_edit_comment_link()),
-                                __('Edit', 'jankx')
-                            );
-                        }
-                        ?>
-                    </div><!-- .comment-metadata -->
-
-                </footer><!-- .comment-meta -->
-
-                <div class="comment-content entry-content">
-
-                    <?php
-
-                    comment_text();
-
-                    if ('0' === $comment->comment_approved) {
-                        ?>
-                        <p class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.', 'jankx'); ?></p>
-                        <?php
-                    }
-
-                    ?>
-
-                </div><!-- .comment-content -->
-
-                <?php
-
-                $comment_reply_link = get_comment_reply_link(
-                    array_merge(
-                        $args,
-                        array(
-                            'add_below' => 'div-comment',
-                            'depth'     => $depth,
-                            'max_depth' => $args['max_depth'],
-                            'before'    => '<span class="comment-reply">',
-                            'after'     => '</span>',
-                        )
-                    )
-                );
-
-                $by_post_author = jankx_is_comment_by_post_author($comment);
-
-                if ($comment_reply_link || $by_post_author) {
-                    ?>
-
-                    <footer class="comment-footer-meta">
-
-                        <?php
-                        if ($comment_reply_link) {
-                            echo $comment_reply_link; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Link is escaped in https://developer.wordpress.org/reference/functions/get_comment_reply_link/
-                        }
-                        if ($by_post_author) {
-                            echo '<span class="by-post-author">' . __('By Post Author', 'jankx') . '</span>';
-                        }
-                        ?>
-
-                    </footer>
-
-                    <?php
-                }
-                ?>
-
-            </article><!-- .comment-body -->
-
-        <?php
+        // Comment body
+        jankx_template(
+            'comment/comment',
+            apply_filters(
+                'jankx_template_comment_item_data',
+                compact(
+                    'args',
+                    'comment',
+                    'depth',
+                    'comment_author_url',
+                    'comment_author',
+                    'avatar',
+                    'comment_timestamp',
+                    'comment_reply_link',
+                    'by_post_author'
+                )
+            )
+        );
     }
 }
