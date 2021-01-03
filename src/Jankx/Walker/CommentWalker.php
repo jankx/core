9e725cc89e21daabeb30c420faba37e3a28d671b
Puleeno Nguyen <puleeno@gmail.com>
2021-01-03 12:17:29 +0700

    Change text domain

--- src/Jankx/Walker/CommentWalker.php
+++ src/Jankx/Walker/CommentWalker.php
@@ -49,7 +49,7 @@ class CommentWalker extends Walker_Comment
                         printf(
                             '<span class="fn">%1$s</span><span class="screen-reader-text says">%2$s</span>',
                             esc_html($comment_author),
-                            __('says:', 'twentytwenty')
+                            __('says:', 'jankx')
                         );
 
                         if (! empty($comment_author_url)) {
@@ -61,7 +61,7 @@ class CommentWalker extends Walker_Comment
                     <div class="comment-metadata">
                         <?php
                         /* translators: 1: Comment date, 2: Comment time. */
-                        $comment_timestamp = sprintf(__('%1$s at %2$s', 'twentytwenty'), get_comment_date('', $comment), get_comment_time());
+                        $comment_timestamp = sprintf(__('%1$s at %2$s', 'jankx'), get_comment_date('', $comment), get_comment_time());
 
                         printf(
                             '<a href="%s"><time datetime="%s" title="%s">%s</time></a>',
@@ -75,7 +75,7 @@ class CommentWalker extends Walker_Comment
                             printf(
                                 ' <span aria-hidden="true">&bull;</span> <a class="comment-edit-link" href="%s">%s</a>',
                                 esc_url(get_edit_comment_link()),
-                                __('Edit', 'twentytwenty')
+                                __('Edit', 'jankx')
                             );
                         }
                         ?>
@@ -91,7 +91,7 @@ class CommentWalker extends Walker_Comment
 
                     if ('0' === $comment->comment_approved) {
                         ?>
-                        <p class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.', 'twentytwenty'); ?></p>
+                        <p class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.', 'jankx'); ?></p>
                         <?php
                     }
 
@@ -126,7 +126,7 @@ class CommentWalker extends Walker_Comment
                             echo $comment_reply_link; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Link is escaped in https://developer.wordpress.org/reference/functions/get_comment_reply_link/
                         }
                         if ($by_post_author) {
-                            echo '<span class="by-post-author">' . __('By Post Author', 'twentytwenty') . '</span>';
+                            echo '<span class="by-post-author">' . __('By Post Author', 'jankx') . '</span>';
                         }
                         ?>
 
