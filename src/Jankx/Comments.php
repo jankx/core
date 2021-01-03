9f0b1faa284e157cc8a8c85ac389e87a1d343d82
Puleeno Nguyen <puleeno@gmail.com>
2021-01-03 10:40:36 +0700

    Load comment form via template hook

--- src/Jankx/Comments.php
+++ src/Jankx/Comments.php
@@ -6,6 +6,7 @@ class Comments
     public static function init()
     {
         add_action('jankx_template_after_content', array(__CLASS__, 'show_comments'));
+        add_action('jankx_template_after_list_comments', array(__CLASS__, 'show_comment_form'));
     }
 
     public static function show_comments()
@@ -19,6 +20,22 @@ class Comments
                 </div><!-- .comments-wrapper -->
 
             <?php
+        }
+    }
+
+    public static function show_comment_form($comments)
+    {
+        if (comments_open() || pings_open()) {
+            if ($comments) {
+                jankx_template('comment/separator');
+            }
+
+            jankx_template('comment/comment-form');
+        } elseif (is_single()) {
+            if ($comments) {
+                jankx_template('comment/separator');
+            }
+            jankx_template('comment/closed-comment');
         }
     }
 }
