9b1879498910908cc39280dc86a8d78a90d50c91
Puleeno Nguyen <puleeno@gmail.com>
2021-05-13 10:35:52 +0700

    Prepare support jankx template system

--- src/Jankx/TemplateLoader.php
+++ src/Jankx/TemplateLoader.php
@@ -8,6 +8,79 @@ class TemplateLoader
 {
     protected static $templateLoaded;
 
+    protected $page;
+    protected $pageType;
+
+    public function __construct()
+    {
+        $this->page     = Page::getInstance();
+        $this->pageType = $this->loadPageType();
+    }
+
+    protected function loadPageType()
+    {
+        $tag_templates = array_unique(
+            array_merge(
+                apply_filters('jankx_custom_template_tags', array()),
+                array(
+                    'is_embed',
+                    'is_404',
+                    'is_search',
+                    'is_front_page',
+                    'is_home',
+                    'is_privacy_policy',
+                    'is_post_type_archive',
+                    'is_tax',
+                    'is_attachment',
+                    'is_single',
+                    'is_page',
+                    'is_singular',
+                    'is_category',
+                    'is_tag',
+                    'is_author',
+                    'is_date',
+                    'is_archive',
+                )
+            )
+        );
+        foreach ($tag_templates as $tag_template) {
+            if (call_user_func($tag_template)) {
+                return = str_replace('is_', '', $tag_template);
+            }
+        }
+    }
+
+    public function initJankxThemeSystem()
+    {
+        /**
+         * Loads the correct template based on the visitor's url
+         *
+         * @package WordPress
+         */
+        do_action('template_redirect');
+
+        /**
+         * Filters the path of the current template before including it.
+         *
+         * @since 3.0.0
+         *
+         * @param string $template The path of the template to include.
+         */
+        $template = apply_filters('template_include', null);
+
+        if (!$template) {
+            return jankx();
+        }
+        include $template;
+    }
+
+    public function generateSearchFiles()
+    {
+        if (!wp_using_themes()) {
+            $this->initJankxThemeSystem();
+        }
+    }
+
     public function load($templateDir)
     {
         Template::getLoader(
@@ -22,10 +95,9 @@ class TemplateLoader
             )
         );
 
-        $pageTemplate = Page::getInstance();
 
-            // Call the Jankx Page
-        add_filter('template_include', array($pageTemplate, 'callTemplate'), 99);
+        // Call the Jankx Page
+        add_action('wp', array($this, 'generateSearchFiles'));
 
         Template::setDefautLoader();
     }
