c976a22d9775657d3316e64ac2546c37a32e3d1f
Puleeno Nguyen <puleeno@gmail.com>
2021-05-15 00:26:42 +0700

    Support set global variable with locked

--- src/Jankx/GlobalVariables.php
+++ src/Jankx/GlobalVariables.php
@@ -6,21 +6,45 @@ namespace Jankx;
  */
 class GlobalVariables
 {
-    protected static $vars;
-    protected static $locked_vars;
+    protected static $vars = array();
+    protected static $locked_vars = array();
 
-    public static function set($var, $value, $lock_it = false)
+    public static function set($name, $value, $lock_it = false)
     {
-        static::$vars[$var] = $value;
+        if (!isset(static::$vars[$name]) || empty(static::$locked_vars[$name])) {
+            $keys = array_reverse(explode('.', $name));
+            foreach ($keys as $index => $key) {
+                if ($index === count($keys) - 1) {
+                    static::$vars[$key] = $value;
+                    break;
+                }
+                $value = array(
+                    $key => $value,
+                );
+            }
 
+            if ($lock_it) {
+                static::$locked_vars[$name] = true;
+            }
+        }
         return true;
     }
 
     public static function get($var, $defaultValue = null)
     {
-        if (isset(static::$vars[$var])) {
-            return static::$vars[$var];
+        $keys = array_filter(explode('.', $var));
+        $current_value = static::$vars;
+        foreach ($keys as $key) {
+            if (!isset($current_value[$key])) {
+                return $defaultValue;
+            }
+            $current_value = $current_value[$key];
         }
-        return $defaultValue;
+        return $current_value;
+    }
+
+    public static function getAll()
+    {
+        return static::$vars;
     }
 }
