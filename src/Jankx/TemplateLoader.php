303a6b0df9d66bcfa4b8439d101789035ba3486d
Puleeno Nguyen <puleeno@gmail.com>
2021-05-18 13:14:45 +0700

    Create new share data `widgets_not_added_message` and update footer-content template

--- src/Jankx/TemplateLoader.php
+++ src/Jankx/TemplateLoader.php
@@ -126,5 +126,15 @@ class TemplateLoader
     public function initSharingData() {
         Data::share('site_name', get_bloginfo('name'));
         Data::share('site_url', site_url());
+
+
+        $widgets_not_added_message = '';
+        if (current_user_can('edit_theme_options')) {
+            $widgets_not_added_message = sprintf(
+                __('Please add the widgets to this sidebar at <a href="%s">Widget Dashboard</a>. Only you see this message because you are the moderator.', 'jankx'),
+                admin_url('widgets.php')
+            );
+        }
+        Data::share('widgets_not_added_message', $widgets_not_added_message);
     }
 }
--- templates/partials/footer-content.php
+++ templates/partials/footer-content.php
@@ -1,4 +1,5 @@
 <footer id="jankx-site-footer" class="jankx-site-footer">
+    <?php do_action('jankx_template_footer_widgets'); ?>
 
     <?php do_action('jankx_template_before_footer_content'); ?>
 
