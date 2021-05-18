f0a1b6bb8fb3f54cf17393f02970a13631523b9f
Puleeno Nguyen <puleeno@gmail.com>
2021-05-18 23:49:39 +0700

    Main sidebar is primary

--- templates/partials/sidebar/primary.php
+++ templates/partials/sidebar/primary.php
@@ -1,12 +1,7 @@
-<?php
-if (!isset($sidebar_name)) {
-    $sidebar_name = 'primary';
-}
-?>
 <aside id="jankx-<?php echo $sidebar_name; ?>-sidebar" class="sidebar <?php echo $sidebar_name; ?>">
     <?php
     if (is_active_sidebar($sidebar_name)) {
-        dynamic_sidebar($sidebar_name);
+        dynamic_sidebar('primary');
     } elseif (current_user_can('edit_theme_options')) {
         printf(
             __('Please add the widgets to this sidebar at <a href="%s">Widget Dashboard</a>. Only you see this message because you are the moderator.', 'jankx'),
