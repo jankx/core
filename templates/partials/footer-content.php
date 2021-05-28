cafb90d174285f0e39fae4d8aa49a66c1a7b286f
Puleeno Nguyen <puleeno@gmail.com>
2021-05-28 21:20:38 +0700

    update footer-content templates

--- templates/partials/footer-content.php
+++ templates/partials/footer-content.php
@@ -3,25 +3,7 @@
 
     <?php do_action('jankx_template_before_footer_content'); ?>
 
-        <?php
-            $site_name = get_bloginfo('name');
-            $site_url = site_url();
-            jankx_component(
-                'footer',
-                array(
-                    'copyright' => sprintf(
-                        __('Copyright &copy; %1$d <a href="%2$s">%3$s</a>.', 'jankx'),
-                        date('Y'),
-                        $site_url,
-                        $site_name
-                    ),
-                    'site_name' => $site_name,
-                    'site_url' => $site_url,
-                ),
-                array(
-                    'echo' => true
-                )
-            ); ?>
+        <?php echo jankx_component('footer'); ?>
 
     <?php do_action('jankx_template_after_footer_content'); ?>
 
