abf94f089e6f7e4d269b2623145c770a02c1cc54
Puleeno Nguyen <puleeno@gmail.com>
2021-07-12 03:23:10 +0700

    Remove common header text template

--- templates/common/header-text.php
+++
@@ -1,7 +0,0 @@
-<h3 class="<?php echo $class_name; ?>">
-    <?php if ($url) : ?>
-        <a href="<?php echo $url; ?>"><?php echo $text; ?></a>
-    <?php else : ?>
-        <?php echo $text; ?>
-    <?php endif; ?>
-</h3>
