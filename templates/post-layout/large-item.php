457446aa32417f64504306aaf9408f9b7548ef11
Puleeno Nguyen <puleeno@gmail.com>
2021-08-21 22:11:36 +0700

    Update post layout template for large loop item

--- templates/post-layout/large-item.php
+++ templates/post-layout/large-item.php
@@ -8,12 +8,31 @@
         </a>
         <?php do_action('jankx_post_layout_after_loop_post_thumbnail', $post); ?>
     </div>
+
     <div class="post-infos">
+        <?php if ($show_title) : ?>
         <h2 class="post-title">
             <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
         </h2>
+        <?php endif; ?>
 
-        <div class="description"><?php the_excerpt(); ?></div>
+        <?php if (!empty($post_meta_features)) : ?>
+            <ul class="post-metas">
+            <?php foreach ($post_meta_features as $feature => $value) : ?>
+                <li class=<?php echo $feature; ?>>
+                <?php
+                    do_action("jankx_post_layout_meta_before_{$feature}");
+
+                    echo $this->e($this->get_meta_value($value, $feature));
+
+                    do_action("jankx_post_layout_meta_after_{$feature}");
+                ?>
+                </li>
+            <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+
+        <div class="post-exceprt"><?php the_excerpt(); ?></div>
     </div>
 
     <?php do_action('jankx_post_layout_after_loop_item', $post); ?>
