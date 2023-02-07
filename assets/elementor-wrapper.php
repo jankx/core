c160e6e4c491f115f909be8b36a37d92bc5cb969
Puleeno Nguyen <puleeno@gmail.com>
2023-02-08 02:10:57 +0700

    update CSS rules

--- assets/elementor-wrapper.php
+++ assets/elementor-wrapper.php
@@ -1,13 +1,13 @@
-.jankx-base .jankx-container {
+.jankx-base .jankx-container.has-wrap {
     max-width: <?php printf('%s%s', $desktop['width'], $desktop['unit']); ?>;
 }
 @media(max-width: <?php echo $breakpoints['lg'] ?>px) {
-    .jankx-base .jankx-container {
+    .jankx-base .jankx-container.has-wrap {
         max-width: <?php printf('%s%s', $tablet['width'], $tablet['unit']); ?>;
     }
 }
 @media(max-width: <?php echo $breakpoints['md'] ?>px) {
-    .jankx-base .jankx-container {
+    .jankx-base .jankx-container.has-wrap {
         max-width: <?php printf('%s%s', $mobile['width'], $mobile['unit']); ?>;
     }
 }
