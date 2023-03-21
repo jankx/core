ad6eb3cecb39c4fb93e0ad5d92a7e46485f74eb3
Puleeno Nguyen <puleeno@gmail.com>
2023-03-21 13:57:17 +0700

    update widget social template

--- templates/widget/socials.php
+++ templates/widget/socials.php
@@ -1,11 +1,11 @@
-<div class="social_sidebar_internal">
-    <?php if (empty($socials)) : ?>
+<div class="social-sidebar-internal">
+    <?php if (!empty($socials)) : ?>
         <ul class="jankx-socials">
             <?php foreach ($socials as $name => $social) : ?>
                 <li <?php echo jankx_generate_html_attributes([
                     'class' => ['social-item', 'social-' . $name],
                 ]); ?>>
-                    <a href="<?php array_get($social, 'url'); ?>" target="<?php echo array_get($social, 'target'); ?>"><i class="<?php echo array_get($social, 'icon') ?>"></i></a>
+                    <a href="<?php echo array_get($social, 'url'); ?>" target="<?php echo array_get($social, 'target'); ?>"><i class="<?php echo array_get($social, 'icon') ?>"></i></a>
                 </li>
             <?php endforeach; ?>
         </ul>
