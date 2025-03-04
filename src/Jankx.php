408f73b61dbb2b200ea4f6767b09945a3a85ee30
Puleeno Nguyen <puleeno@gmail.com>
2025-03-04 15:01:32 +0700

    Use object-fit to keep original ratio of image

--- src/Jankx.php
+++ src/Jankx.php
@@ -51,7 +51,7 @@ use function ElementorDeps\DI\get;
 class Jankx extends Container
 {
     const FRAMEWORK_NAME    = 'Jankx Framework';
-    const FRAMEWORK_VERSION = '1.0.0.22';
+    const FRAMEWORK_VERSION = '1.0.0.28';
     const ENGINE_ID         = 'jankx';
 
     protected static $instance;
