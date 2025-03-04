40899a52aa79078b09f91b5cb6aba4203eaeb3a4
Puleeno Nguyen <puleeno@gmail.com>
2025-03-04 15:37:15 +0700

    change post thumbnail hook name

--- templates/post-layout/thumbnail.php
+++ templates/post-layout/thumbnail.php
@@ -1,7 +1,7 @@
 <div <?php echo jankx_generate_html_attributes([
     'class' => apply_filters('jankx/thumbnail/classes', ['post-thumbnail'], $post, $data_index),
 ]); ?>>
-    <?php do_action('jankx_post_layout_before_loop_post_thumbnail', $post, $data_index); ?>
+    <?php do_action('jankx/post_layout/thumbnail/before', $post, $data_index); ?>
     <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
         <?php if (empty($content)): ?>
         <?php jankx_the_post_thumbnail($thumbnail_size); ?>
@@ -9,5 +9,5 @@
             <?php echo $content; ?>
         <?php endif; ?>
     </a>
-    <?php do_action('jankx_post_layout_after_loop_post_thumbnail', $post, $data_index); ?>
+    <?php do_action('jankx/post_layout/thumbnail/after', $post, $data_index); ?>
 </div>
