ff0765f0e21dca2395e2e883a04e0f7c77221276
Puleeno Nguyen <puleeno@gmail.com>
2025-05-23 18:35:51 +0700

    create place holder tempaltes for recommended posts

---
+++ templates/recommeded/posts.php
@@ -0,0 +1,6 @@
+<div class="jankx-recommended-posts-display">
+    <h2><?php esc_html_e(' Có thể bạn quan tâm: ', 'jankx'); ?></h2>
+    <div class="recommended-posts-grid">
+        <?php echo $content; ?>
+    </div>
+</div>
